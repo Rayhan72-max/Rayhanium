d6c8eeed2d1968bc843ae52098d0cf6c7bd5c0b6
Rayhan72-max <raypatwary72@gmail.com>
2020-09-28 14:54:33 +0600

    Updates

---
+++ precache-manifest.ff3eba9a208e29c2dafb28b7ab026aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165fd643519579daefe22079c29e9f4f",
+    "url": "/Rayhanium/index.html"
+  },
+  {
+    "revision": "1bb8edab488e3ba7f6c8",
+    "url": "/Rayhanium/static/css/main.6fade7b6.chunk.css"
+  },
+  {
+    "revision": "0300c6f42915ffe344cb",
+    "url": "/Rayhanium/static/js/2.a62abc36.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Rayhanium/static/js/2.a62abc36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb8edab488e3ba7f6c8",
+    "url": "/Rayhanium/static/js/main.e2f095b5.chunk.js"
+  },
+  {
+    "revision": "aa6ca4e3e5189062156b",
+    "url": "/Rayhanium/static/js/runtime-main.e2151a97.js"
+  }
+]);
